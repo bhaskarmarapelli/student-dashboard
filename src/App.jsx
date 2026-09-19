51ccb42877024fac8dc824359a1a62f17f06d522
bhaskarmarapelli <bhaskarmarapelli@gmail.com>
2026-09-19 09:43:44 +0530

    react app created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Welcome to react My name is Bhaskar</h1>
+      <h2>this is secondday work</h2>
+    </>
+  )
+}
+
+export default App
